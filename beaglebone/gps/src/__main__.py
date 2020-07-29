dee0101e088b99c8f6bf641f6d90a8e1e1112923
Polishdudealan <atondryk@umich.edu>
2020-07-28 20:23:35 -0400

    [GPS] Polished up code with regards to data needs from auton. Removed quality, num of satellites, and altitude from lcm struct. Formatted datatypes (latitude degrees to int16_t) and struct variable names (groundSpeed -> speed)

--- beaglebone/gps/src/__main__.py
+++ beaglebone/gps/src/__main__.py
@@ -2,6 +2,7 @@ import serial
 import lcm
 from rover_msgs import GPSData
 import struct
+import numpy as np
 lcm_ = lcm.LCM()
 
 baud = 38400
@@ -89,30 +90,36 @@ def main():
                 # dddmm.mmmm format (degrees, minutes, minutes/seconds
                 if(datalist[3] != ""):
                     if(datalist[4] == "N"):
-                        gps.latitude_deg = int(float(datalist[3]) / 100)
+                        gps.latitude_deg = np.int16(int(float(datalist[3]) / 100))
                         gps.latitude_min = float(datalist[3]) % 100
+                        gps.latitudeDirection = "N"
                     elif(datalist[4] == "S"):
-                        gps.latitude_deg = int(float(datalist[3]) / 100) * -1.0
+                        gps.latitude_deg = np.int16(int(float(datalist[3]) / 100) * -1.0)
                         gps.latitude_min = float(datalist[3]) % 100
+                        gps.latitudeDirection = "S"
                 else:
                     gps.latitude_deg = 0
                     gps.latitude_min = 0
+                    gps.latitudeDirection = "-"
                 if(datalist[5] != ""):
                     if(datalist[6] == "W"):
-                        gps.longitude_deg = int(float(datalist[5]) / 100)
+                        gps.longitude_deg = np.int16(int(float(datalist[5]) / 100))
                         gps.longitude_min = float(datalist[5]) % 100
+                        gps.longitudeDirection = "W"
                     elif(datalist[6] == "E"):
-                        gps.longitude_deg = int(float(datalist[5]) / 100) * -1
+                        gps.longitude_deg = np.int16(int(float(datalist[5]) / 100) * -1.0)
                         gps.longitude_min = float(datalist[5]) % 100
+                        gps.longitudeDirection = "E"
                 else:
                     gps.longitude_deg = 0
                     gps.longitude_min = 0
+                    gps.longitudeDirection = "-"
                 if(datalist[8] != ''):
                     # degrees
-                    gps.bearing_deg = float(datalist[8])
+                    gps.trackAngle_deg = float(datalist[8])
                 else:
                     # degrees
-                    gps.bearing_deg = 0
+                    gps.trackAngle_deg = 0
 
             elif(datalist[0] == "$GNVTG"):
                 # print("Transmission Type:", datalist[0],
@@ -126,34 +133,34 @@ def main():
                     gps.speed = float(datalist[7])
                 else:
                     gps.speed = 0
-            elif(datalist[0] == "$GNGGA"):
+            # elif(datalist[0] == "$GNGGA"):
                 # print("Transmission Type:", datalist[0],
                 # "Fix Quality(4=RTK):", datalist[6],
                 # "Number of satellites tracked:", datalist[7],
                 # "Altitude (above mean sea level):", datalist[9],
                 # "time since last DGPS update (s):", datalist[11])
                 # 0-4
-                if(datalist[6] != ""):
-                    gps.quality = int(datalist[6], 10)
-                else:
-                    gps.quality = 0
+                # if(datalist[6] != ""):
+                    # gps.quality = int(datalist[6], 10)
+                # else:
+                    # gps.quality = 0
 
                 # mean sea level altitude in meters
-                if(datalist[9] != ""):
-                    gps.altitude = float(datalist[9])
-                else:
-                    gps.altitude = 0
-            elif(datalist[0] == "$GPGSV"):
+                # if(datalist[9] != ""):
+                    # gps.altitude = float(datalist[9])
+                # else:
+                    # gps.altitude = 0
+            # elif(datalist[0] == "$GPGSV"):
                 # print("Transmission Type:", datalist[0],
                 # "# of sentences for full data:", datalist[1],
                 # "Sentence _ of total:", datalist[2],
                 # "# satellites in view:", datalist[3]
 
                 # int
-                if(datalist[3] != ""):
-                    gps.satellitesInView = int(datalist[3], 10)
-                else:
-                    gps.satellitesInView = 0
+                # if(datalist[3] != ""):
+                    # gps.satellitesInView = int(datalist[3], 10)
+                # else:
+                    # gps.satellitesInView = 0
             if(tempTimeStamp != gps.timeStamp):
                 # publishes data
                 # print("tempTime:", tempTimeStamp, "tStamp:", gps.timeStamp)
