ec4974610ed2500d52ef715f48edfdfa9f83b97a
Demo User <debian@beaglebone.localdomain>
2020-09-24 00:40:28 +0000

    Changed subscriptions to new auton lcm structs and finalized code (verified it correctly sent bytes to arduino)

--- beaglebone/led/src/__main__.py
+++ beaglebone/led/src/__main__.py
@@ -1,32 +1,38 @@
 import serial
 import lcm
-from rover_msgs import LEDCmd
-import struct
+from rover_msgs import AutonState, NavStatus
 import Adafruit_BBIO.UART as UART
 lcm_ = lcm.LCM()
 
 baud = 9600
 
-def led_callback(channel, msg):
-    led = LEDCmd.decode(msg)
 
-    if ser.isOpen():
-        try:
-            if led.mode == "A":
-                ser.write("A".encode('utf-8'))
-            elif led.mode == "M":
-                ser.write("M".encode('utf-8'))
-            elif led.mode == "L":
-                ser.write("L".encode('utf-8'))
-            else:
-                print("Unable to read lcm struct")
-        except ser.SerialTimeoutException:
-            print("write timeout")
-    else:
-        print("Serial is not open")
+def nav_status_callback(channel, msg):
+    state = NavStatus.decode(msg)
+    with serial.Serial(port="/dev/ttyS4", baudrate=baud) as ser:
+        if ser.isOpen():
+            try:
+                if led.state == "A":
+                    ser.write("A".encode('utf-8'))
+                elif led.state == "M":
+                    ser.write("M".encode('utf-8'))
+                elif led.state == "Done":
+                    ser.write("D".encode('utf-8'))
+                else:
+                    print("Unable to read lcm struct")
+            except ser.SerialTimeoutException:
+                print("write timeout")
+        else:
+            print("Serial is not open")
+
+def auton_state_callback(channel, msg):
+    state = AutonState.decode(msg)
+    if(state.is_auton):
+        lcm_.subscribe("/NavStatus", nav_status_callback)
+
 
 def main():
-    lcm_.subscribe("/led_cmd", led_callback)
+    lcm_.subscribe("/AutonState", auton_state_callback)
     UART.setup("UART4")
     led = LED()
     with serial.Serial(port="/dev/ttyS4", baudrate=baud) as ser:
